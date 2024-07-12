f2de8df5b0a7e651ddecf7335d38632a3f2ca185
Fady Mohsen <fadymohsen62@gmail.com>
2024-07-12 23:21:08 +0300

    Personal info Page

---
+++ Personal Profile Page/profilePage.js
@@ -0,0 +1,28 @@
+
+// Select elements
+var logo = document.querySelector(".logo");
+var profilePhoto = document.querySelector(".profile-photo");
+var userName = document.querySelector(".username");
+var fullName = document.querySelector(".fullname");
+var userGender = document.querySelector(".gender");
+var userEmail = document.querySelector(".email");
+var userPhone = document.querySelector(".phone");
+var userPurchase = document.querySelector(".purchase");
+
+// Redirect to cat.html when logo is clicked
+logo.addEventListener("click", function() {
+    window.location.href = 'cat.html';
+});
+
+
+
+
+// For retrieve data and post it to the fields
+
+// profilePhoto.style.background = `url(${})`;
+// userName.innerText = 
+// fullName.innerText = 
+// userGeneder.innerText =
+// userEmail.innerText =
+// userPhone.innerText =
+// userPurchase.innerHTML = 
