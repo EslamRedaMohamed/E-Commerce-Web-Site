7608884fe201768918bb134b5560b49c386ff5f8
EslamRedaMohamed <eslamreda.info@gmail.com>
2024-07-12 21:08:37 +0300

    add price discount and review to product page

--- product.js
+++ product.js
@@ -25,12 +25,45 @@ console.log(fetchProductById(getProductIdFromURL()))
 async function displayProductDetails() {
     var productId = getProductIdFromURL();
     var product = await fetchProductById(productId);
+    var discountedPrice = product.price-Math.round(product.price*product.discountPercentage/100);
 
     document.getElementById('product-image').src = product.thumbnail;
     document.getElementById('product-title').textContent = product.title;
     document.getElementById('product-brand').textContent ='Brand: '+product.brand;
     document.getElementById('product-description').textContent = product.description;
-    document.getElementById('product-price').textContent = `Price: ${product.price} EGP`;
+    document.getElementById('product-price').textContent = `Price: ${discountedPrice} EGP`;
+    document.getElementById('old-price').textContent = `Price: ${product.price} EGP`;
+    document.getElementById('discoutPercentage').textContent = `-${product.discountPercentage}%`;
+
+    function displayProductReviews(reviews) {
+        var reviewsContainer = document.getElementById('product-reviews');
+        reviewsContainer.innerHTML = ''; // Clear previous reviews
+    
+        reviews.forEach(function(review) {
+            // Generate HTML for star ratings
+            var ratingHtml = '';
+            for (let i = 0; i < 5; i++) {
+                if (i < review.rating) {
+                    ratingHtml += '<i class="bx bxs-star"></i>';
+                } else {
+                    ratingHtml += '<i class="bx bx-star"></i>';
+                }
+            }
+    
+            // Generate HTML for the review
+            var reviewHtml = `
+                <div class="review">
+                    <p id="user-name">${review.reviewerName}</p>
+                    <div id="user-rating" class="rating">${ratingHtml}</div>
+                    <p>${review.comment}</p>
+                    <p><strong>Date:</strong> ${new Date(review.date).toLocaleDateString()}</p>
+                </div>
+            `;
+            reviewsContainer.innerHTML += reviewHtml;
+        });
+    }
+
+    displayProductReviews(product.reviews);
 
 
 
