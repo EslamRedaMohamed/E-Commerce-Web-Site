0e10efe71e063c72f14ef3b4a136269d847094c4
Fady Mohsen <fadymohsen62@gmail.com>
2024-07-13 23:30:41 +0300

    x

---
+++ profilePage.js
@@ -0,0 +1,34 @@
+
+var logo = document.querySelector(".logo")
+var profilePhoto = document.querySelector(".profile-photo")
+var userName = document.querySelector(".username")
+var fullName = document.querySelector(".fullname")
+var userBth = document.querySelector(".bth")
+var userCountry = document.querySelector(".country")
+var userState = document.querySelector(".state")
+var userAddress = document.querySelector(".address")
+var userGeneder = document.querySelector(".geneder")
+var userEmail = document.querySelector(".email")
+var userPhone = document.querySelector(".phone")
+var userPurchase = document.querySelector(".purchase")
+
+
+logo.addEventListener("click", function(){
+    window.location.href = 'cat.html';
+})
+
+
+
+
+// profilePhoto.style.background = `url(${})`;
+// userName.innerText = 
+// fullName.innerText = 
+// userBth.innerText =
+// userCountry.innerText =
+// userState.innerText = 
+// userAddress.innerText =
+// userGeneder.innerText =
+// userGeneder.innerText =
+// userEmail.innerText =
+// userPhone.innerText =
+// userPurchase.innerHTML = 
